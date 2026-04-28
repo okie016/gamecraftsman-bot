ed8d66964ca5e7e87fa37e415f17b44815418378
okie016 <okie016@gmail.com>
2026-04-28 18:52:36 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,8 +21,8 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.command(name="ทำปก")
 async def make_cover(ctx, *, title: str):
     # --- [ส่วนตั้งค่าดีไซน์ - แก้ตรงนี้ได้เลย] ---
-    FONT_SIZE = 80             
-    TEXT_Y_POSITION = 850      # ปรับความสูงต่ำที่ตัวเลขนี้
+    FONT_SIZE = 87             
+    TEXT_Y_POSITION = 1090      # ปรับความสูงต่ำที่ตัวเลขนี้
     MAIN_COLOR = (255, 255, 255, 255)      
     HIGHLIGHT_COLOR = (188, 234, 47, 255)   # สีเหลืองทอง
     # -------------------------------------
