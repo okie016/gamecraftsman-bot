142c50b313152947bf304822d033031393cf8ffe
okie016 <okie016@gmail.com>
2026-04-29 00:31:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ async def make_cover(ctx, *, title: str):
 
     # -------- Design Config --------
     FONT_SIZE = 95
-    LINE_SPACING = 24
+    LINE_SPACING = 26
     START_Y = 990
 
     MAIN_COLOR = (255, 255, 255, 255)
